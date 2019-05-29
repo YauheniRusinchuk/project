c2ebc7492a6cce7f77da0dd4d052e53634a29b49
YauheniRusinchuk <ruevgal@gmail.com>
2019-05-29 13:17:04 +0300

    add fixed all pages date

--- src/models/announcement/models.py
+++ src/models/announcement/models.py
@@ -8,7 +8,10 @@ class Announcement(models.Model):
     author  = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title   = models.CharField(max_length=300, blank=False)
     text    = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-created']
 
 
     def __str__(self):

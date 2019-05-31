c9758ae0ffb39b976a619506338c0f47dadd6d83
YauheniRusinchuk <ruevgal@gmail.com>
2019-05-31 18:32:18 +0300

    test class createpost

--- src/apps/announcement/urls.py
+++ src/apps/announcement/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListAnnouncement, DetailView
+from .views import ListAnnouncement, DetailView, CreateAnnouncement
 
 app_name = 'announcement'
 
 urlpatterns = [
+    path('create/', CreateAnnouncement.as_view(), name='announcement_create'),
     path('article<int:pk>/', DetailView.as_view(), name='announcement_detail'),
     path('', ListAnnouncement.as_view(), name='announcement_list'),
 ]
--- src/apps/announcement/views.py
+++ src/apps/announcement/views.py
@@ -1,6 +1,13 @@
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from src.models.announcement.models import Announcement
 from django.views.generic.detail import DetailView
+
+
+class CreateAnnouncement(CreateView):
+    model = Announcement
+    template_name = 'announcement/create.html'
+    fields = '__all__'
 
 
 class ListAnnouncement(ListView):
